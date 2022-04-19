a8470b8fcd62cfeea1adadd023d1f62b04dadd42
Ghita Aaddaj <aa.ghita@gmail.com>
2022-04-19 21:27:07 +0200

    application works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Le Grand Bain", phone_number:"09 83 02 72 02", address: "14 rue denoyez", category: "french")
+Restaurant.create(name: "L'Avant Poste", phone_number:"09 81 41 01 07", address: "7 rue de la fidélité", category: "french")
+Restaurant.create(name: "Le Cheval d'Or", phone_number:"09 54 12 21 77", address: "21 rue de la villette", category: "japanese")
+Restaurant.create(name: "Les Resistants", phone_number:"01 77 32 77 61", address: "16-18 rue du château d'eau", category: "french")
+Restaurant.create(name: "Daily Syrien", phone_number:"09 54 11 75 35", address: "55 rue du faubourg saint-denis", category: "french")
